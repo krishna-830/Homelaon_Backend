1ea044633fcf7c0696ee45c9c1ed8d26387ba59d
krishna-830 <krishnawadle830@gmail.com>
2023-02-21 18:26:32 +0530

    created Account head

---
+++ Account_Head_Service/src/test/java/com/myhomeloan/AccountHeadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myhomeloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountHeadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
