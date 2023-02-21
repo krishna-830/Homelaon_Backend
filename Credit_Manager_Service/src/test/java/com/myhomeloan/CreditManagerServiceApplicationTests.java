c1689e04694e3acf5188acf97e5451a9a9430191
krishna-830 <krishnawadle830@gmail.com>
2023-02-21 18:48:25 +0530

    created credit manager

---
+++ Credit_Manager_Service/src/test/java/com/myhomeloan/CreditManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myhomeloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
