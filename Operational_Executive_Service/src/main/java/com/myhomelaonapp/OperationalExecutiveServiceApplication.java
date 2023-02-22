e47b589b7b07dde2c89581a4d1f0b833bdbf5790
krishna-830 <krishnawadle830@gmail.com>
2023-02-22 10:46:37 +0530

    created operational executive

---
+++ Operational_Executive_Service/src/main/java/com/myhomelaonapp/OperationalExecutiveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myhomelaonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationalExecutiveServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationalExecutiveServiceApplication.class, args);
+	}
+
+}
