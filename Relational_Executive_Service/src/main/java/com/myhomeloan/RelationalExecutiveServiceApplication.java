b6a17127973f51c362b9803d9126b19294f5a1ef
sourabh-09 <sourabhbhukan09@gmail.com>
2023-02-22 11:13:51 +0530

    sourabh commited

--- Relational_Executive_Service/src/main/java/com/myhomeloan/RelationalExecutiveServiceApplication.java
+++ Relational_Executive_Service/src/main/java/com/myhomeloan/RelationalExecutiveServiceApplication.java
@@ -8,7 +8,7 @@ public class RelationalExecutiveServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RelationalExecutiveServiceApplication.class, args);
-	System.out.println("hii krishna");
+
 	}
 
 }
