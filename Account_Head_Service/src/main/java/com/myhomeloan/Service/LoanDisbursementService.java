da1e31e17e97aa3420eb7d4d63818b0b74db1a4f
Digambar16 <digambarwaybhase@gmail.com>
2023-02-24 19:24:54 +0530

    updated Account Head service

--- Account_Head_Service/src/main/java/com/myhomeloan/Service/LoanDisbursementService.java
+++
@@ -1,9 +0,0 @@
-package com.myhomeloan.Service;
-
-import com.myhomeloan.model.LoanDisbursement;
-
-public interface LoanDisbursementService {
-
-	LoanDisbursement createLoanDisbursementFile(LoanDisbursement loan);
-
-}
--- Account_Head_Service/src/main/java/com/myhomeloan/ServiceIMPL/SantionServiceIMPL.java
+++
@@ -1,9 +0,0 @@
-package com.myhomeloan.ServiceIMPL;
-
-import org.springframework.stereotype.Service;
-
-import com.myhomeloan.Service.SantionService;
-@Service
-public class SantionServiceIMPL implements SantionService {
-
-}
